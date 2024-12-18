7bd3ba64cdd5944d62fa011d18daf806265f7988
Erildo07 <erildonunes12gmail.com>
2024-12-17 23:08:06 -0300

    first commit

---
+++ lista.py
@@ -0,0 +1,16 @@
+frase = 'Olá, tudo bem?'
+lista_nomes = ['Erildo Nunes', 'João', 'Maria', 'Lorena', 'Ghost']
+
+# Adicionando novos elementos à lista
+lista_nomes.append('Julio')
+lista_nomes.append('Juliana')
+lista_nomes.append('SSH')
+
+# Removendo um elemento específico
+lista_nomes.remove('João')
+
+# Inserindo um elemento em uma posição específica
+lista_nomes.insert(1, 'Creosvaldo')
+
+# Imprimindo a lista final
+print(lista_nomes)
